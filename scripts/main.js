ebdf663f68d7bdb32a5183209efba56fd5ef213f
Your Name <nandasafiqalfiansyah@gmail.com>
2023-06-28 18:21:44 +0700

    v0.0

--- scripts/main.js
+++ scripts/main.js
@@ -9,8 +9,37 @@ const firebaseConfig = {
   measurementId: "G-DKBL14E0JS",
 };
 // Inisialisasi Firebase
-
 firebase.initializeApp(firebaseConfig);
+
+// Menambahkan event listener pada tombol unduh
+var downloadButton = document.getElementById("downloadButton");
+downloadButton.addEventListener("click", function () {
+  // Mendapatkan referensi ke database Firebase
+  var database = firebase.database();
+
+  // Mendapatkan referensi ke folder "riwayat" di database
+  var riwayatRef = database.ref("riwayat");
+
+  // Mendownload data dari folder "riwayat"
+  riwayatRef.once("value").then(function (snapshot) {
+    var riwayatData = snapshot.val();
+
+    // Mengkonversi data menjadi format yang sesuai untuk didownload (misalnya JSON)
+    var riwayatJSON = JSON.stringify(riwayatData);
+
+    // Membuat tautan untuk mengunduh data
+    var downloadLink = document.createElement("a");
+    downloadLink.href =
+      "data:text/plain;charset=utf-8," + encodeURIComponent(riwayatJSON);
+    downloadLink.download = "riwayat.json";
+
+    // Memicu klik pada tautan untuk memulai unduhan
+    downloadLink.click();
+
+    // Menampilkan notifikasi setelah data diunduh
+    alert("Data riwayat berhasil diunduh");
+  });
+});
 
 function performSearch() {
   var searchInput = document.getElementById("search-input").value.toLowerCase(); // Mengubah menjadi huruf kecil untuk pencarian yang tidak case-sensitive
@@ -180,12 +209,192 @@ firebase.auth().onAuthStateChanged((user) => {
           userData.imageUrl || ""; // Menampilkan gambar profil
         document.getElementById("updated-profile-images").src =
           userData.imageUrl || ""; // Menampilkan gambar profil
+      }
 
-        // Pembaruan komponen lainnya
-        // ...
+      //chart
+      const database = firebase.database();
+
+      // Mendengarkan perubahan data pada node 'Hasil_Pembacaan'
+      database.ref("Hasil_Pembacaan").on("value", function (snapshot) {
+        var data = snapshot.val();
+        var suhu = data ? data.suhu : null;
+        var kelembapan = data ? data.kelembapan : null;
+
+        // Memperbarui nilai suhu dan kelembapan pada elemen HTML
+        var suhuElement = document.getElementById("suhu");
+        var kelembapanElement = document.getElementById("kelembapan");
+
+        if (suhuElement && kelembapanElement) {
+          suhuElement.innerHTML = suhu !== null ? suhu + " °C" : "-";
+          kelembapanElement.innerHTML =
+            kelembapan !== null ? kelembapan + " %" : "-";
+        }
+
+        // Menggambar grafik lingkaran
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myChart = new Chart(ctx, {
+          type: "doughnut",
+          data: {
+            labels: ["Suhu", "Kelembapan"],
+            datasets: [
+              {
+                data: [suhu, kelembapan],
+                backgroundColor: ["#0088cc", "#ffaa00"],
+                borderColor: "#ffffff",
+                borderWidth: 2,
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+              position: "bottom",
+              labels: {
+                fontColor: "#333333",
+              },
+            },
+          },
+        });
+      });
+    });
+
+    // Mendengarkan perubahan data pada node 'Hasil_Pembacaan'
+    database.ref("Hasil_Pembacaan").on("value", function (snapshot) {
+      var data = snapshot.val();
+      var suhu = data ? data.suhu : null;
+      var kelembapan = data ? data.kelembapan : null;
+
+      // Menghitung persentase width berdasarkan data suhu
+      var suhuPersentase = suhu !== null ? (suhu / 100) * 100 : 0;
+
+      // Mengubah style width pada elemen div progress bar suhu
+      var suhuProgressBar = document.getElementById("suhuProgressBar");
+      if (suhuProgressBar) {
+        suhuProgressBar.style.width = suhuPersentase + "%";
+        suhuProgressBar.setAttribute("aria-valuenow", suhuPersentase);
+      }
+
+      // Menghitung persentase width berdasarkan data kelembapan
+      var kelembapanPersentase =
+        kelembapan !== null ? (kelembapan / 100) * 100 : 0;
+
+      // Mengubah style width pada elemen div progress bar kelembapan
+      var kelembapanProgressBar = document.getElementById(
+        "kelembapanProgressBar"
+      );
+      if (kelembapanProgressBar) {
+        kelembapanProgressBar.style.width = kelembapanPersentase + "%";
+        kelembapanProgressBar.setAttribute(
+          "aria-valuenow",
+          kelembapanPersentase
+        );
       }
     });
 
+    database.ref("riwayat").on("value", function (snapshot) {
+      var data = snapshot.val();
+      var riwayatArray = [];
+
+      // Mengubah objek data menjadi array
+      if (data) {
+        riwayatArray = Object.keys(data).map(function (key) {
+          return {
+            timestamp: key,
+            suhu: data[key].suhu,
+            kelembapan: data[key].kelembapan,
+          };
+        });
+      }
+
+      // Memperbarui nilai suhu dan kelembapan pada elemen HTML
+      var suhuElement = document.getElementById("suhu");
+      var kelembapanElement = document.getElementById("kelembapan");
+
+      if (suhuElement && kelembapanElement) {
+        var latestData =
+          riwayatArray.length > 0
+            ? riwayatArray[riwayatArray.length - 1]
+            : null;
+        suhuElement.innerHTML = latestData ? latestData.suhu + " °C" : "-";
+        kelembapanElement.innerHTML = latestData
+          ? latestData.kelembapan + " %"
+          : "-";
+      }
+
+      // Menghapus dan menginisialisasi tabel menggunakan plugin DataTables
+      var table = $("#measurementTable").DataTable({
+        destroy: true, // Menghancurkan tabel sebelumnya (jika ada)
+        data: riwayatArray,
+        columns: [
+          {
+            data: "timestamp",
+            render: function (data) {
+              return new Date(parseInt(data)).toLocaleString();
+            },
+          },
+          { data: "suhu" },
+          { data: "kelembapan" },
+        ],
+      });
+    });
+
+    //UPDATE DATA HISTORY SETIAP 1 JAM
+
+    const storageRef = firebase.storage().ref();
+
+    // Mendefinisikan nama folder
+    const folderName = "data";
+
+    // Memanggil fungsi saveHistory setiap satu jam
+    setInterval(function () {
+      // Mendapatkan data suhu dan kelembapan dari node 'Hasil_Pembacaan'
+      database.ref("Hasil_Pembacaan").once("value", function (snapshot) {
+        var data = snapshot.val();
+        var suhu = data ? data.suhu : null;
+        var kelembapan = data ? data.kelembapan : null;
+
+        // Menyimpan data ke Firebase Database
+        saveHistory(suhu, kelembapan);
+      });
+    }, 60 * 60 * 1000); // Setiap satu jam (60 menit x 60 detik x 1000 milidetik)
+
+    function saveHistory(suhu, kelembapan) {
+      // Menyimpan riwayat pengukuran ke Firebase Database
+      const database = firebase.database();
+      const historyRef = database.ref("riwayat");
+      const timestamp = Date.now();
+
+      historyRef
+        .child(timestamp)
+        .set({
+          suhu: suhu,
+          kelembapan: kelembapan,
+        })
+        .then(() => {
+          console.log("History saved successfully");
+
+          // Menyimpan data ke folder "data" di Firebase Storage
+          const dataString = JSON.stringify({
+            suhu: suhu,
+            kelembapan: kelembapan,
+          });
+          const fileRef = storageRef.child(
+            folderName + "/" + timestamp + ".json"
+          );
+          fileRef
+            .putString(dataString, "raw")
+            .then(() => {
+              console.log("Data saved to folder successfully");
+            })
+            .catch((error) => {
+              console.log("Failed to save data to folder:", error);
+            });
+        })
+        .catch((error) => {
+          console.log("Failed to save history:", error);
+        });
+    }
     // Lanjutkan dengan penggunaan userRef untuk akses ke data pengguna
   } else {
     // Pengguna belum terotentikasi, lakukan penanganan sesuai kebutuhan
