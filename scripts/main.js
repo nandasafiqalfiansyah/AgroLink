ce7533f4daed5ecb04a2da1e6c399e5457b2822f
Your Name <nandasafiqalfiansyah@gmail.com>
2023-07-08 12:53:24 +0700

    v0.1

--- scripts/main.js
+++ scripts/main.js
@@ -614,7 +614,7 @@ firebase.auth().onAuthStateChanged((user) => {
 
       setInterval(function () {
         updateData();
-      }, 180000);
+      }, 300000);
 
       const updateBtn = document.getElementById("updateBtn");
       updateBtn.addEventListener("click", updateData);
